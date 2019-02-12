ed87e3477fbf8f0e518e8cbcbc2b6711da2974ee
Hezi-D <dddavid1984@gmail.com>
2019-02-13 01:11:10 +0200

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,11 @@ import SearchBar from "./SearchBar";
 import "./mainStyle.css";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
   //State, init with empty array
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async termFromUser => {
     const response = await youtube.get("/search", {
@@ -17,12 +18,22 @@ class App extends React.Component {
 
     this.setState({ videos: response.data.items });
   };
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+    console.log("this is the video", video);
+  };
+
   render() {
     return (
       <div className="ui container">
         <h1>HeziTube</h1>
         <SearchBar whenTermSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,9 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedlist = videos.map(video => {
-    return <VideoItem video={video} />;
+    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
   });
 
   return <div className="ui realxed divided list">{renderedlist}</div>;
